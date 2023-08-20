8c1423b911a41e83c9a38e2ae643c50f1507110b
Muhammad Burhan Ahmed <burhan.ahmed60090@gmail.com>
2023-08-20 20:42:56 +0500

    Updated Login Page

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -2,20 +2,21 @@ import React from "react";
 
 export default function Login() {
     return (
-        <div className="mx-auto m-52 p-28 w-1/2 flex justify-center shadow-xl bg-slate-400">
-            <div>
-            https://giphy.com/gifs/ash-ketchum-pokemon-pikachu-7ISIRaCMrgFfa            </div>
-            <div className="p-28">
-                <h1 className="text-center font-semibold text-4xl">
+        <div className="mx-auto my-64 w-6/12 h-5/6 rounded-lg flex justify-center shadow-2xl bg-slate-300">
+            <div >
+                <img className="rounded-s-lg" src="src\assets\ezgif.com-resize.gif" alt="Animated GIF" />
+            </div>
+            <div className="px-28 p-24 text-center">
+                <h1 className="mb-5 font-semibold text-5xl">
                     Pokédex
                 </h1>
-                <p>
-                    Wellcome to Pokédex
+                <p className="mb-12 font-mono text-md">
+                    Welcome to Pokédex
                 </p>
-                <form className="flex flex-col" action="GET">
+                <form className="flex flex-col space-y-2" action="GET">
                     <input className="py-2 ps-5 rounded focus:outline-none" placeholder="Enter Email" type="email" name="" id="" />
                     <input className="py-2 ps-5 rounded focus:outline-none" placeholder="Enter your Password" type="text" name="" id="" />
-                    <button className="py-1 px-3 border border-black rounded">
+                    <button className="py-1 px-3 hover:text-white hover:bg-black ease-in-out border border-black rounded">
                         Login
                     </button>
                 </form>
