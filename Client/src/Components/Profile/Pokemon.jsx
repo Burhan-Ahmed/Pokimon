58862940588b990fbcaeb99588c022d65f3b5253
Muhammad Burhan Ahmed <burhan.ahmed60090@gmail.com>
2023-08-27 23:09:25 +0500

    Updates

--- Client/src/Components/Profile/Pokemon.jsx
+++ Client/src/Components/Profile/Pokemon.jsx
@@ -1,18 +1,3 @@
-import React from "react";
-import './profile.css'
-<<<<<<< HEAD
-
-export default function Pokemon() {
-    return (
-        <div className="contain mx-auto border border-black">
-            <div className=" p-5 border border-black">
-                LIST
-            </div>
-            <div className="p-5 border border-black">
-                Pokemon
-            </div>
-        </div>
-=======
 import PokiCard from "./Cards";
 import { Container } from "@mui/material";
 
@@ -46,6 +31,5 @@ export default function Pokemon() {
                 </div>
             </Container>
         </>
->>>>>>> 375aaaa70b8af6f3e951eeb78958956074382993
     );
 }
